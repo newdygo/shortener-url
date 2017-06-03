35c373dc0df7d7136ffc9a5cb9219dae4e0579af
newdygo <diego.balduino.vip@gmail.com>
2017-06-03 19:02:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(/api, function (req, res, next) {
-  next();
-});
-
 app.route('/api').post(function(req, res) {
     
     //console.log('Starting...');
