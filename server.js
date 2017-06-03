7260eebe862faee14d9e2a8686d6d37522a707e8
newdygo <diego.balduino.vip@gmail.com>
2017-06-03 18:00:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,93 @@
 var express = require('express');
-var bodyParser = require("body-parser");
-var mongo = require('mongodb').MongoClient;
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectId
 var app = express();
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+var connectionString = 'mongodb://newdygo:newdygo99894517@ds029436.mlab.com:29436/shortener-url-api';
 
-app.use(bodyParser.json());
+app.use(/new\/(http|https|ftp|ftps)\:\/\//, function (req, res, next) {
+  next();
+});
 
-app.route('/api').get(function(req, res) {
+app.use(/new\/*/, function (req, res, next) {
+  next();
+});
+
+app.use('/:id', function (req, res, next) {
+  next();
+});
+
+app.route(/new\/(http|https|ftp|ftps)\:\/\//).get(function (req, res) {
+ 
+  var url = req.url.replace('new/', '');
+  
+  if (/(http(s)?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w- ;,.\/?%&=]*)?/.test(url) || /(ftp(s)?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w- ;,.\/?%&=]*)?/.test(url)) {
     
-    console.log('Starting...');
+    mongodb.connect(connectionString, function(err, db) {
     
-    //mongo.connect('mongodb://admin_conductor:jUBeKXUd2GTcvlAL@cdtmongo-shard-00-02-btirt.mongodb.net:27017/MailGun?ssl=true&replicaSet=CDTMongo-shard-0&authSource=admin', function(err, db) {
-    //                    
-    //    if (!err) {
-    //
-    //        var eventt = {
-    //                city: req.body.city,
-    //                domain: req.body.domain,
-    //                recipient: req.body.recipient,
-    //                event: req.body.event,
-    //                timestamp: req.body.timestamp,
-    //                token: req.body.token,
-    //                signature: req.body.signature
-    //        };
-    //
-    //        db.collection('events').insert(eventt);
-    //    }
-    //
-    //    db.close();
-    //});
+      if (!err) {
+        
+        db.collection('urls').insert({url: url}, function(err2, data) {
+                  
+          if (data.ops[0] !== undefined) {
+            
+            var url_res = {original_url: url, short_url: 'https://ionit-fcc-shortener-url.herokuapp.com//' + data.ops[0]._id};
+            
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(url_res));
+            res.end();
+          }
+        });
+        
+        db.close();
+      }
+    });
+  }
+});
+
+app.route(/new\/*/).get(function (req, res) {
+  
+  res.writeHead(404, {'Content-Type': 'application/json'});
+  res.write('INVALID URL!!');
+  res.end();
+  
+});
+
+app.route('/:id').get(function (req, res) {
+  
+  var id = req.url.replace('/', '');
+  
+  if (id.length === 12 || id.length === 24) {
     
-    res.status(200);
-    res.send({message: "OK"});
+    mongodb.connect(connectionString, function(err, db) {
     
+      if (!err) {
+        
+        db.collection('urls').find({
+            _id: new objectId(id)
+          
+        }).toArray(function(err2, data) {
+          
+          if (data[0] !== undefined) {
+            
+            res.redirect(data[0].url);
+          }
+          else {
+            
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.write('INVALID URL!!');
+            res.end();
+          }
+        });
+      }
+    });
+  }
+  else {
+    
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    res.write('INVALID URL!!');
+    res.end();
+  }
 });
 
 app.listen(process.env.PORT || 8080);
